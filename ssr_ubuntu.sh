e3b23d8839d24d154a3cae29be0344bd8f94f917
wangbingyan90 <1418885488@qq.com>
2017-10-21 13:12:30 +0800

    ubuntu

---
+++ ssr_ubuntu.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo "下载程序"
+git clone https://github.com/wangbingyan90/ssr.git
+
+echo "配置程序"
+
+cd ssr/
+
+chmod +x *.sh
+chmod +x shadowsocks/*.sh
+cp -n apiconfig.py userapiconfig.py
+cp -n config.json user-config.json
+cp -n mysql.json usermysql.json
+
+
+echo "启动程序"
+cd shadowsocks/
+
+python server.py -d start
+
+
+
