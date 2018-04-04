4f39d862e485b8ac2450ab1fa68c34c05c2dfa08
wangbingyan90 <1418885488@qq.com>
2018-04-04 19:57:59 +0800

    Update jdk.sh

--- jdk.sh
+++ jdk.sh
@@ -3,9 +3,14 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+echo "安装wget"
+yum -y install wget
+
 echo "下载JDK"
 wget wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"  http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.rpm
 
 echo "安装JDK"
-
 rpm -ivh jdk-8u161-linux-x64.rpm
+
+echo "安装完成"
+java -version
