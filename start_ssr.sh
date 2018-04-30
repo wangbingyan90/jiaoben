45f73c62f10639a69c0a1c8b0916736cbb51ca6f
wangbingyan90 <1418885488@qq.com>
2018-04-30 14:45:00 +0800

    Update start_ssr.sh

--- start_ssr.sh
+++ start_ssr.sh
@@ -4,6 +4,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 echo "启动程序"
-cd ssr/hadowsocks/
+
+cd /root/jiaoben/ssr/shadowsocks/
 
 python server.py -d start
