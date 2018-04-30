f5a75ca49d535e97f1617ec4bbdf336f4c61c9e9
wangbingyan90 <1418885488@qq.com>
2018-04-30 21:17:01 +0800

    upinstallgit

---
+++ install_git.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo "安装wget"
+yum -y install wget
+
+echo "下载git2.17"
+wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.17.0.tar.gz
+
+echo "安装依赖库"
+yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel
+
+yum install -y asciidoc xmlto docbook2x
+
+echo "解压"
+tar -zxf git-2.17.0.tar.gz 
+cd git-2.17.0/
+
+echo "配置"
+ make configure
+./configure --prefix=/usr
+
+echo "编译"
+make all doc info
+
+echo "安装"
+make install install-doc install-html install-info
+
+
+echo "安装完成"
+git --version
